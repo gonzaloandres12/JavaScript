8338004ef14111bd7fbbc21d8f042cefea2a53dc
gonzalo12-10 <gonzalo.andres@liceosorollab.es>
2022-11-24 17:42:08 +0100

    añadido eliminar

--- javaScript/Objetos/js/biblocteca.js
+++ javaScript/Objetos/js/biblocteca.js
@@ -19,15 +19,14 @@ function funcionGuardar() {
 function funcionVisualizar() {
     document.getElementById('textArea').value = "";
     console.log(biblio);
-    let i = 0;
     console.log(biblio[0].autor);
+    console.log(biblio.length);
     for (let index = 0; index < biblio.length; index++) {
-        document.getElementById('textArea').value += "Titulo:" + biblio[i].titulo + "\n";
-        document.getElementById('textArea').value += "Autor:" + biblio[i].autor + "\n";
-        document.getElementById('textArea').value += "Edtorial:" + biblio[i].editorial + "\n";
-        document.getElementById('textArea').value += "Fecha:" + biblio[i].fecha + "\n";
-
-
+        document.getElementById('textArea').value += "Titulo:" + biblio[index].titulo + "\n";
+        document.getElementById('textArea').value += "Autor:" + biblio[index].autor + "\n";
+        document.getElementById('textArea').value += "Edtorial:" + biblio[index].editorial + "\n";
+        document.getElementById('textArea').value += "Fecha:" + biblio[index].fecha + "\n";
+        
     }
 }
 
